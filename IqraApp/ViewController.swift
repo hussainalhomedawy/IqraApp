63327dcfbc348273e59b2c23c7997a2c0036f02c
Hussain Al-Homedawy <hussain_al@live.ca>
2016-12-04 18:04:11 -0500

    Initial Commit

---
+++ IqraApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IqraApp
+//
+//  Created by Hussain Al-Homedawy on 2016-12-04.
+//  Copyright © 2016 Hussain Al-Homedawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
